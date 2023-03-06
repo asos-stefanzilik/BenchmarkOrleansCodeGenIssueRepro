92d3c10deee1e79fa0ca20873da9864efea10025
Stefan Zilik <stefan.zilik@asos.com>
2023-03-06 17:04:58 +0000

    Add project files.

---
+++ BenchmarkOrleansTest/BenchmarkGrain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkOrleansTest
+{
+    public interface IBenchmarkGrain : IGrainWithStringKey
+    {
+        ValueTask<int> DoStuff();
+    }
+    public class BenchmarkGrain : IBenchmarkGrain
+    {
+        public async ValueTask<int> DoStuff()
+        {
+            return 1;
+        }
+    }
+}
---
+++ BenchmarkOrleansTest/OrleansBenchmark.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkOrleansTest
+{
+    public class OrleansBenchmark
+    {
+        private IHost host;
+        private IGrainFactory grainFactory;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            this.host = new HostBuilder()
+               .UseOrleans(siloBuilder =>
+               {
+                   siloBuilder.UseLocalhostClustering();
+               })
+               .Build();
+
+            this.grainFactory = host.Services.GetRequiredService<IGrainFactory>();
+        }
+
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            this.host?.Dispose();
+        }
+
+        [Benchmark]
+        public async Task Benchmark()
+        {
+            var benchmarkGrain = this.grainFactory.GetGrain<IBenchmarkGrain>("test");
+            await benchmarkGrain.DoStuff();
+        }
+    }
+}
